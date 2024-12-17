e4baa882a064859992c9de9ef08db7baccc68413
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2024-12-18 05:03:42 +0900

    Title: 함수를 이용한 윤년 판별, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 241218/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
+++ 241218/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.java
@@ -9,6 +9,7 @@ public class Main {
     public static boolean isLeap(int y){
         if(y%4 == 0 && y%100 != 0) return true;
         if(y%100 == 0 && y%400 != 0 ) return false;
+        if(y%100 == 0) return true;
         return false;
     }
 }
