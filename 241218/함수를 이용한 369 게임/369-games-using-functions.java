957dcde53011468901857a344540d82633564505
Hayoung Moon <124586544+gkdudans@users.noreply.github.com>
2024-12-18 04:35:29 +0900

    Title: 함수를 이용한 369 게임, Time: 162ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241218/함수를 이용한 369 게임/369-games-using-functions.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(countNum(a,b));
+    }
+
+    public static int countNum(int a, int b){
+        int num = 0;
+        for(int i=a; i<=b; i++){
+            if(hasOneOfThem(i)){
+                num++;
+            } 
+            else if(isMultipleOfThree(i)){
+                num++;
+            } 
+            else continue;
+        }
+        return num;
+    }
+
+    public static boolean hasOneOfThem(int n){ 
+        for(int i=0; i<6; i++){
+            if(n == 0) break;
+            if((n%10)%3 == 0) return true;
+            n /= 10; 
+        }
+        return false;
+    }
+
+    public static boolean isMultipleOfThree(int sum){
+        return (sum%3) == 0;
+    }
+}
